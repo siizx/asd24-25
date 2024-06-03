7b1939e667e6b29ebc56d036ca882cfc6caf4079
Andrea <siizx@live.it>
2024-06-03 11:19:49 +0200

    general update

---
+++ asd24-25/asd-lab1-traccia/ASD-doubly-linked-list.cpp
@@ -0,0 +1,139 @@
+// FILE DA MODIFICARE
+#include <iostream>
+#include <fstream>
+#include "ASD-doubly-linked-list.h"
+
+using namespace list;
+using namespace std;
+
+/*Descrizione da come e' fatta  la struttura node. */
+/*I dettagli interni non saranno visibili dal main*/
+struct list::node {  
+  Elem info;
+  node *prev;
+  node *next;
+};
+
+/**************************************************/
+/*      funzioni da implementare                    */
+/**************************************************/
+  
+/* crea la lista vuota */
+void list::createEmpty(List& li){
+	//TO DO
+}
+
+/* "smantella" la lista (tranne la sentinella) */
+void list::clear(List& li){
+	//TO DO
+}
+
+/* restituisce true se la lista e' vuota */
+bool list::isEmpty(const List& li){
+  //TO DO
+  return false;
+}
+
+/* restituisce la dimensione della lista */
+unsigned int list::size(const List& li){
+	//TO DO
+  return 9999;
+}
+
+/* restituisce l'elemento in posizione pos */
+/* se pos non e corretta, solleva una eccezione di tipo string*/
+Elem list::get(unsigned int pos, const List& li){
+	//TO DO
+  return 7777;
+}
+
+/* modifica l'elemento in posizione pos */
+/* se pos non e' corretta, solleva una eccezione di tipo string*/
+void list::set(unsigned int pos, Elem el, const List& li){
+	//TO DO
+}
+
+/* inserisce l'elemento in posizione pos*/
+/*shiftando a destra gli altri elementi */
+/*se pos non e' corretta, solleva una eccezione di tipo string*/
+void list::add(unsigned int pos, Elem el, const List& li){
+	//TO DO
+}
+
+/* inserisce l'elemento alla fine della lista */
+void list::addRear(Elem el, const List& li){
+	//TO DO
+}
+
+/* inserisce l'elemento all'inizio della lista */
+void list::addFront(Elem el, const List& li) {
+	//TO DO
+}
+
+/* cancella l'elemento in posizione pos dalla lista */
+/* se pos non e' corretta, solleva una eccezione di tipo string*/
+void list::removePos(unsigned int pos,const List& li){
+	//TO DO
+}
+
+/* cancella tutte le occorrenze dell'elemento elem*/
+/*se presenti, dalla lista */
+void list::removeEl(Elem el,const List& li){
+	//TO DO
+}
+
+/**************************************************/
+/*      funzioni implementate                    */
+/**************************************************/
+  
+/* riempie una lista da file */
+void list::readFromFile(string name_file, const List& li){
+  ifstream infile;
+  string read_data;
+  infile.open(name_file);
+  while (getline (infile,read_data)){
+    if(read_data.length()>0){
+      addRear(stoi(read_data),li);
+    }
+  }
+}
+
+/* riempie una lista da standard input */
+void list::readFromStdin(const List& li){
+  cout << "Inserire una sequenza di numeri separati da spazi seguiti da END per terminare\n";
+  string read_data;
+  cin>>read_data;
+  while(read_data!="END"){
+    addRear(stoi(read_data),li);
+    cin>>read_data;
+  }
+}
+
+/* stampa la lista */
+void list::print(const List& li){
+  if(li==li->next){
+    cout<<"vuota";
+  }
+  List cur = li->next;  // q "salta la sentinella" e va alla testa della lista
+  while (cur != li) {   
+    cout << cur->info;
+    cur= cur->next;
+    if(cur!=li) cout<<" <-> ";
+  }
+  cout<<endl;
+}
+
+/* produce una string contenendo la lista*/
+std::string list::tostring(const List& li){
+  string out="";
+  if(li==li->next){
+    out+="vuota";
+  }
+  List cur = li->next;  // q "salta la sentinella" e va alla testa della lista
+  while (cur != li) {   
+    out+=to_string(cur->info);
+    cur= cur->next;
+    if(cur!=li) out+=" <-> ";
+  }
+  return out;
+}
